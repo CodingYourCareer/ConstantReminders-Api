733853609f72a58b297c1f116f7b72d9c698b753
Jahi Lewis <jahifortislewis@gmail.com>
2024-11-24 13:13:20 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using Amazon.CloudWatchLogs;
+using Serilog;
+using Serilog.Sinks.AwsCloudWatch;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+
+using var log = new LoggerConfiguration().MinimumLevel.Verbose()
+.WriteTo.Console().CreateLogger();
+
+log.Verbose("Preparing for development...");
+log.Information("Hi there! How are you ?");
+
+
+app.MapGet("/health", () => new {status = "healthy"});
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,1 @@
+# ConstantReminders-Api
