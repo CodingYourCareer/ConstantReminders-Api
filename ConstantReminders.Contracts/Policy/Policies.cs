752352521986af8497ee8d67e98246387275409b
Jay Durocher <biskyniffer@gmail.com>
2025-02-18 15:27:26 -0500

    scaffolding-done

---
+++ ConstantReminders.Contracts/Policy/Policies.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantReminders.Contracts.Policy;
+
+//This is where we will store the strings used to identify individual policies
+public static class Policies
+{
+    public const string OwnerPolicy = "OwnerPolicy";
+}
