fcbed324d7e70a633fbc66589b63436a29208a6c
Jay Durocher <biskyniffer@gmail.com>
2025-02-23 13:15:47 -0500

    updated-to-show-all-code-no-tests-yet

---
+++ ConstantReminders.Api/Policies/Owner Policy/OwnerPolicyHandler.cs
@@ -0,0 +1,23 @@
+﻿using ConstantReminders.Contracts.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace ConstantReminder.Api.Policies
+{
+    public class OwnerPolicyHandler : AuthorizationHandler<OwnerPolicyRequirement, User>
+    {
+        /*used to ensure only the user associated with a task can make changes to that task*/
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, OwnerPolicyRequirement requirement, User resource)
+        {
+            var userId = context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value ??
+                         context.User.FindFirst("sub")?.Value;
+
+            if (userId == resource.CreatedBy)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
